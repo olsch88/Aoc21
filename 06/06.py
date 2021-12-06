44751223d08f0f9f316e0d54a9e27ebf1bbb64a6
olsch88 <95592115+olsch88@users.noreply.github.com>
2021-12-06 22:01:00 +0100

    Add files via upload

---
+++ 06/06.py
@@ -0,0 +1,37 @@
+from typing import List
+
+with open("06_input.txt", "r") as source:
+    input = source.readline()
+
+start_sample = [3,4,3,1,2]
+
+def create_counter_list(init_list: List[int]) -> List[int]:
+    counter_list = [0]*9
+    for i in init_list:
+        counter_list[i]+=1
+    return counter_list
+
+def process_counter_list(current_day: List[int]) -> List[int]:
+    nex_day = [0]*9
+    for index, count in enumerate(current_day):
+        if index == 0:
+            nex_day[8] += count
+            nex_day[6] += count
+        else:
+            nex_day[index-1] += count
+    return nex_day
+
+fish_list = [int(x) for x in input.split(",")]
+fish_counter_list = create_counter_list(fish_list)
+
+# Part 1:
+for i in range(80):
+    fish_counter_list = process_counter_list(fish_counter_list)
+print("Solution Part 1:\n{}\n".format(sum(fish_counter_list)))
+
+# Part 2:
+# reset list
+fish_counter_list = create_counter_list(fish_list)
+for i in range(256):
+    fish_counter_list = process_counter_list(fish_counter_list)
+print("Solution Part 2:\n{}\n".format(sum(fish_counter_list)))
