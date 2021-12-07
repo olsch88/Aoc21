4b2f4bd8d0f1f27b8f23fd53942c86a939c9a9d4
olsch88 <95592115+olsch88@users.noreply.github.com>
2021-12-07 22:26:15 +0100

    Add files via upload

---
+++ 07/07.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+with open("07_input.txt", "r") as file:
+    data_raw = file.readline()
+
+data = np.array([int(x) for x in data_raw.split(",")])
+
+middle = np.median(data)
+
+fuel = 0
+for i in data:
+    fuel += np.abs(i-middle)
+print("Solution Part 1:")
+print(fuel)
+
+def gauss_sum(n: int)-> int:
+    return n*(n+1)/2
+
+# data = np.array([16,1,2,0,4,2,7,1,2,14])
+
+min_fuel_cost = 99999999999
+optimal_pos = 0
+for i in range(max(data)):
+    fuel_cost = 0
+    for crab in data:
+        fuel_cost += gauss_sum(np.abs(crab-i))
+    if fuel_cost <= min_fuel_cost:
+        min_fuel_cost = fuel_cost
+        optimal_pos = i
+    if fuel_cost > min_fuel_cost:
+        break
+
+print("Solution Part 2:")
+print("Optimal Position:")
+print(optimal_pos)
+print("Total Fuel Cost:")
+print(min_fuel_cost)
